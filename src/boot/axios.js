e960734bfa9ab1f2e5116d582507706cd3433718
Ezeqqi <ezequiel.l.silveira@gmail.com>
2022-08-08 16:58:27 -0300

    feat: config new events API and remove json-server

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,15 +1,15 @@
 import { boot } from "quasar/wrappers";
 import axios from "axios";
 
-const apiHoliday = axios.create({ baseURL: 'https://brasilapi.com.br/api/feriados/v1/' })
-const apiEvents = axios.create({ baseURL: 'http://localhost:3000/' })
-
+//const apiHoliday = axios.create({ baseURL: 'https://brasilapi.com.br/api/feriados/v1/' })
+const apiEvents = axios.create({ baseURL: 'https://calendar-vuejs-api.herokuapp.com', debug: true })
+// https://calendar-vuejs-api.herokuapp.com/events
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios
 
-  app.config.globalProperties.$api = apiHoliday
+  //app.config.globalProperties.$api = apiHoliday
 
   app.config.globalProperties.$api = apiEvents
 })
 
-export { apiEvents, apiHoliday };
+export { apiEvents };
--- src/services/eventsService.js
+++ src/services/eventsService.js
@@ -1,7 +1,7 @@
 import useApiEvents from 'src/composables/useApiEvents.js'
 
 export default function eventsService() {
-  const { getEvents, postEvents, putEvents, removeEvents, getById } = useApiEvents('DbEvents')
+  const { getEvents, postEvents, putEvents, removeEvents, getById } = useApiEvents('/events')
 
   return {
     getEvents,
